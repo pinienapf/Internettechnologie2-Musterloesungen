63b29e9ce6c71de6a72fdf56fb4392222d293113
Andreas Baumgart <andreas.baumgart@jade-hs.de>
2023-03-16 12:32:01 +0100

    --

--- vorlesungswoche3/ergebnis.php
+++ vorlesungswoche3/ergebnis.php
@@ -10,17 +10,37 @@
 	<?php
 		echo "Hallo, ".$_POST["vorname"]." ".$_POST["nachname"];
 		// Formularinhalte entgegen nehmen
+		$vorname=$_POST["vorname"];
 		$zahl1 = doubleval($_POST["zahl1"]);
 		$zahl2 = doubleval($_POST["zahl2"]);
 		// Überprüfung, ob Zahl 1 kleiner Zahl 2 ist
-		if($zahl1<$zahl2)
+		/* Vergleichsoperatoren
+			== gleich
+			!= ungleich
+			< kleiner als
+			> größer als
+			<= kleiner oder gleich
+			>= größer oder gleich
+			
+			
+			Logische Operatoren
+			Logische Oder ||
+			Logische UND &&
+		*/
+	
+	// Wenn Zahl 1 kleiner Zahl 2  und Vorname gleich Andreas ist
+	
+		 if(($zahl1<$zahl2)&&($vorname=="Andreas"))
+	
+		//if(($zahl1<$zahl2)&&($zahl1==5))
 			{
 				$ergebnis = $zahl1+$zahl2;
 				echo "<br> Die Addition von $zahl1 und $zahl2 ergibt $ergebnis<br>";
-			}else
-				{
-					echo "<p>Zahl 1 muss kleiner Zahl 2 sein</p>";
-				}
+			}else 
+		 			{
+						echo "<p>Zahl 1 muss kleiner Zahl 2 sein</p>";
+					} 
+					
 	
 	
 		
--- vorlesungswoche3/index.php
+++ vorlesungswoche3/index.php
@@ -7,13 +7,15 @@
 
 <body>
 	<h1>Arbeiten mit Formularen</h1>
-	<form action="ergebnis.php" method="post">
+	<form action="index.php" method="post">
 		<p><input name="vorname">Vorname</p>
 		<p><input name="nachname">Nachname</p>
 		<p><input name="zahl1">zahl1</p>
 		<p><input name="zahl2">zahl2</p>
 		<p><input type="submit"</p>
 	</form>
+	
+	
 	
 </body>
 </html>
