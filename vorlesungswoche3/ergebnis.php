7f3f3c11770e7535d0b93292a398daadb31dd7c8
Andreas Baumgart <andreas.baumgart@jade-hs.de>
2023-03-16 11:02:26 +0100

    --

--- vorlesungswoche3/ergebnis.php
+++ vorlesungswoche3/ergebnis.php
@@ -9,11 +9,21 @@
 	<h1>Ergebnisseite</h1>
 	<?php
 		echo "Hallo, ".$_POST["vorname"]." ".$_POST["nachname"];
-		
+		// Formularinhalte entgegen nehmen
 		$zahl1 = doubleval($_POST["zahl1"]);
 		$zahl2 = doubleval($_POST["zahl2"]);
-		$ergebnis = $zahl1+$zahl2;
-		echo "<br> Die Addition von $zahl1 und $zahl2 ergibt $ergebnis<br>";
+		// Überprüfung, ob Zahl 1 kleiner Zahl 2 ist
+		if($zahl1<$zahl2)
+			{
+				$ergebnis = $zahl1+$zahl2;
+				echo "<br> Die Addition von $zahl1 und $zahl2 ergibt $ergebnis<br>";
+			}else
+				{
+					echo "<p>Zahl 1 muss kleiner Zahl 2 sein</p>";
+				}
+	
+	
+		
 	
 	
 		// Umwandlung von Zeichenketten in Zahlen
