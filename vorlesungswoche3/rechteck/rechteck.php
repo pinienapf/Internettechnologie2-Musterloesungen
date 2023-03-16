6c41ec9ae6bde0478b7472413ebeb8512f0e3755
Andreas Baumgart <andreas.baumgart@jade-hs.de>
2023-03-16 22:49:51 +0100

    --

--- vorlesungswoche3/rechteck/rechteck.php
+++ vorlesungswoche3/rechteck/rechteck.php
@@ -6,36 +6,65 @@
 </head>
 
 <body>
+	<h1>Rechteckberechnung</h1>
 	<?php
-		// Dies ist ein Kommentar.
-		
-		/* Ein mehrzeiliger Kommentar, 
-		der wieder mit einen Sternchen gefolgt vom Schrägstrich geschlossen werden muss.
-		*/
-		// Definition der benötigten Variablen für die Gleichung 
+		// Das Ergebnis wird erst angezeigt wenn das Formular gesendet wurde. Details siehe weiter unten.
+		if(isset($_POST["gesendet"]))
+		{
 	
-		$hoehe=60;
-		$breite=1.53;
-		$umbruch="<br>";
-		
-		// Berechnung des Umfangs  
-		$umfang = (2*$breite)+(2*$hoehe);
-		// Ausgabe des Umfangs
-		echo "Umfang: ".$umfang;
-		echo $umbruch; // Zeilenumbruch, damit die Zahlen nicht hintereinander stehen
-		
-		// Berechnung der Fläche
-		$flaeche = $breite*$hoehe;
-		// Ausgabe der Fläche
-		echo "Fläche: ".$flaeche;
-		echo $umbruch; // Zeilenumbruch
-	
-		// Berechnung der Diagonalen
-		$diagonale = sqrt(pow($breite,2)+pow($hoehe,2));
-		// Ausgabe der Diagonalen
-		echo "Diagonale: ".$diagonale;
-		echo $umbruch; // Zeilenumbruch
-	
+			$seiteA=doubleval($_POST["seiteA"]);
+			$seiteB=doubleval($_POST["seiteB"]);
+			
+
+
+			if($seiteA!=0 && $seiteB!=0)
+			{
+				$umfang = (2*$seiteA)+(2*$seiteB);
+				$flaeche= $seiteA*$seiteB;
+				$diagonale = sqrt(pow($seiteA,2)+pow($seiteB,2));
+				$ergebnis = "Ein Rechteck der Seitelänge a=$seiteA und der Seitelänge $b hat einen Umfang von $umfang, eine Fläche von $flaeche sowie eine Diagonale von $diagonale";
+				
+
+			} else
+			{
+				$ergebnis ="Die Berechnung konnte nicht durchgeführt werden. Eine oder beide Seitenlängen dürfen nicht Null sein.";
+				
+			}
+		}
 	?>
+	
+	<?php 
+				/* Das Ergebnis wird erst angezeigt wenn das Formular gesendet wurde. Ebenfalls die Berechnung (Siehe oben).
+				Sonst wird die Berechnung ausgeführt, obwohl noch keine Zahlen erfasst oder eine Auswahl getroffen wurde.
+				Dafür wird der Submit-Button mit einem Namen versehen "gesendet". Dieser kann jetzt mit isset abgefragt werden.
+				*/
+				if(isset($_POST["gesendet"]))
+					{
+						echo $ergebnis;
+					}
+		?>
+	
+	
+	<form action="rechteck.php" method="post">	
+		<p>
+			<label for="seiteA">Seite A</label>
+			<input id="seiteA" name="seiteA"> 
+			
+			<label for="seiteB">Seite B</label>
+			<input id="seiteB" name="seiteB"> 
+			
+			
+		</p>
+		
+		<p><input type="submit" name="gesendet"</p>
+	</form>
+	
+	
+	<?php 
+			// Einbinden einer php-Datei, die Code enthält, der sonst redundant wäre.
+			
+			include '../backLink_inc.php'; 
+	?>
+	
 </body>
 </html>
